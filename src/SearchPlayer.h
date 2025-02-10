bde6a32d3f130fd3b9dd725f6b5b8d5f5caacc34
HoangThiThaoNhi <119788935+HoangThiThaoNhi@users.noreply.github.com>
2025-02-10 09:06:50 +0700

    # Định nghĩa lớp tìm kiếm người chơi

---
+++ src/SearchPlayer.h
@@ -0,0 +1,14 @@
+#ifndef _SEARCH_PLAYER_H
+#define _SEARCH_PLAYER_H
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cstdlib>
+#include "Player.h"
+
+void enterPlayerAccount(std::string &account);
+
+void searchAccount(std::string &account);
+
+#endif
